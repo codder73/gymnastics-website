15944adc7931da59ed1078f6ca04154c2e8921ae
“coder73” <codder73@gmail.com>
2023-08-07 15:39:40 +0530

    edited techincals page

---
+++ pages/technical/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Technical 	from '../../components/sections/index/technical'
+
+
+export default function tech() {
+  return (
+    <>
+			<Technical />
+    </>
+  )
+}
